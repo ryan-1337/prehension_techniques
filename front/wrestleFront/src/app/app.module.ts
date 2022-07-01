886052348e46abc3020582491c75cc4bc5368a17
ryan-1337 <ryanaouiche@gmail.com>
2022-07-01 19:31:48 +0200

    starting angular

--- front/wrestleFront/src/app/app.module.ts
+++ front/wrestleFront/src/app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const router: Routes = [
+  { path: 'home', component: IndexComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(router),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
